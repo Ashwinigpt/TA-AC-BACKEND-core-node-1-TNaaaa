100fdeb05fe1895e3ce7b84887a0effaf47b8599
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-03-21 11:53:32 +0530

    block-BNaaat

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -4,14 +4,14 @@ let fs = require('fs');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-    if(req.method === 'GET' && req.url === '/') {
-        res.setHeader('Content-Type', 'text/html');
+    if(req.method === 'GET' && req.url === '/file') {
         fs.readFile('./node.html', (err, content) => {
             if (err) console.log(err);
+            res.setHeader('Content-Type', 'text/html');
             res.end(content);
         })
     }
-    if(req.method === 'GET' && req.url === '/about') {
+    if(req.method === 'GET' && req.url === '/stream') {
         res.setHeader('Content-Type', 'text/html');
         fs.createReadStream('./node.html').pipe(res);
     }
